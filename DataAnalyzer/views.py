e28a6c459bec378194b51ec3e860e5e4bb1c9cd5
oalberto96 <oalberto96c@gmail.com>
2017-10-22 23:37:44 -0700

    Refresh page every 1

--- DataAnalyzer/views.py
+++ DataAnalyzer/views.py
@@ -16,13 +16,39 @@ def post_list(request):
     return HttpResponse(humidity,temperature);
 
 def post_model_list(request):
-    sensor_data = SensorData.objects.all()
+    sensor_data = SensorData.objects.all().order_by('-date')
+    actuators = get_actuators(sensor_data)
     template='monitor.html'
     context = {
         'sensor_data': sensor_data
     }
+    context.update(actuators)
     return render(request,template,context)
 
 def get_list_data(request):
-    sensor_data = SensorData.objects.all()
-    return render(request, 'list_view.html', {'sensor_data': sensor_data})
+    sensor_data = SensorData.objects.all().order_by('-date')
+    actuators = get_actuators(sensor_data)
+    context = {
+        'sensor_data': sensor_data
+    }
+    context.update(actuators)
+    return render(request, 'list_view.html',context)
+
+def get_actuators(query):
+    actuators = {}
+    latest = query[0]
+    if latest.temperature < 25:
+        actuators['fan'] = "Desactivado"
+        actuators['light'] = "Activado"
+    else:
+        if latest.temperature>=25 and latest.temperature <=30:
+            actuators['fan'] = "Desactivado"
+            actuators['light'] = "Desactivado"
+        elif latest.temperature > 30:
+            actuators['fan'] = "Desactivado"
+            actuators['light'] = "Desactivado"
+    if latest.humidity < 60:
+        actuators['water'] = "Activado"
+    else:
+        actuators['water'] = "Desactivado"
+    return actuators
--- post_script.py
+++ post_script.py
@@ -2,19 +2,3 @@
 import requests
 r = requests.post("http://10.0.0.2:8000/arduinodata", data={'humidity': 5.5, 'temperature':79.4})
 print(r.text)
-        <!---<script>
-            function refresh() {
-                $.ajax({
-                    url: {% url 'list_view' %},
-                    success: function(data) {
-                        $('#data').html(data);
-                    }
-                });
-                setInterval("refresh()", 10000);
-            }
-
-            $(function(){
-                refresh();
-            });
-            setInterval(updateTable,10000);
-        </script>--->
